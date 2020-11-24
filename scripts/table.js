a0a9b5bc7ae8334a5b473d04693a31597fbcf04f
barbara prusiewicz <barbara.prusiewicz@gmail.com>
2020-11-23 19:20:37 -1000

    More table functionality

--- scripts/table.js
+++ scripts/table.js
@@ -22,47 +22,78 @@ class ControlPanel {
   }
 
   loadHandler() {
-    this.loadButton.prop('disabled', true);
     this.collapseButton.prop('disabled', false);
+    this.collapseButton.html("Collapse Columns");
     this.filterButton.prop('disabled', false);
     this.sortButton.prop('disabled', false);
     this.table.load()
   }
 
   sortHandler() {
-    console.log('sort table')
+    this.table.sort()
   }
 
   filterHandler() {
-    console.log('filter table')
+    this.table.filter()
   }
 
   collapseHandler() {
-    console.log('collapse table')
+    this.table.collapse()
+    const currentButton = this.collapseButton.text();
+    this.collapseButton.text( currentButton==="Collapse Columns" ? "Expand Columns" : "Collapse Columns")
   }
 } 
 
 class Table {
   constructor () {
     this.table = $("#table")
-    this.data = [{a:1,b:2,c:3},{a:4,b:5,c:6},{a:7,b:8,c:9},{a:10,b:11,c:12}]
+    this.baseData = [{a:1,b:2,c:3},{a:40,b:5,c:6},{a:75,b:8,c:9},{a:10,b:11,c:12}]
+    this.data = []
+  }
+
+  addHeader = () => {
+    let row = $("<tr/>");
+    row.append($("<th class='a'>").text('A'));
+    row.append($("<th class='b'>").text('B'));
+    row.append($("<th class='c'>").text('C'));
+    this.table.append(row);
+  }
+
+  addRows = () => {
+    let row;
+    this.data.forEach( r => {
+      row = $("<tr/>");
+      row.append($("<td class='a'>").text(r.a));
+      row.append($("<td class='b'>").text(r.b));
+      row.append($("<td class='c'>").text(r.c));      
+      this.table.append(row);
+    });
   }
 
   load () {
+    this.data=[...this.baseData]
     this.table.empty();
-    let row = $("<tr/>");
-    row.append($("<th>").text('A'));
-    row.append($("<th>").text('B'));
-    row.append($("<th>").text('C'));
-    this.table.append(row);
+    this.addHeader();
+    this.addRows()     
+  }
 
-    this.data.forEach( r => {
-      row = $("<tr/>");
-      row.append($("<td>").text(r.a));
-      row.append($("<td>").text(r.b));
-      row.append($("<td>").text(r.c));      
-      this.table.append(row);
-    });      
+  sort () {
+    this.table.empty();
+    this.addHeader();
+    this.data.sort( (a,b) => a.a - b.a )
+    this.addRows()
+  }
+
+  filter () {
+    this.table.empty();
+    this.addHeader();
+    this.data = this.data.filter ( r => r.b%2===1)
+    this.addRows()
+  }
+
+  collapse() {
+   // $("#table .b").hide();
+    $("#table .b").toggle();
   }
 }
 
